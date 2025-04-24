9b37c1eb7d97da61f7df616361dc9e2a2938b976
chenryanm <ryan.chen649585@tufts.edu>
2025-04-24 19:05:14 -0400

    added files

---
+++ stockTicker.js
@@ -0,0 +1,58 @@
+const app = require('express')();
+const {MongoClient} = require('mongodb');
+
+const url = "mongodb+srv://ryanchen649585:Rmcmongodb05@cluster0.uqmqa6r.mongodb.net/";
+
+const databaseName = "Stock";
+
+let collection;
+
+async function connectToMongoDB(){
+    const client = await MongoClient.connect(url);
+    
+    try {
+        const db = client.db(databaseName);
+        collection = db.collection("PublicCompanies");
+        console.log("Connected to MongoDB Atlas");
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB Atlas:", error);
+    }
+    
+}
+
+connectToMongoDB(); 
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('stockTickerForm');
+});
+
+app.get('/process', async (req, res) => {
+    const query = req.query.query;
+    const searchType = req.query.searchType;
+
+    let results = [];
+
+    try {
+        if (searchType === 'ticker'){
+            results = await collection.find({ticker: query}).toArray();
+        } else if (searchType === 'company'){
+            results = await collection.find({company: {$regex: query, $options: 'i'}}).toArray();
+        }
+
+        results.forEach(company => {
+            console.log(`Company: ${company.company}, Ticker: ${company.ticker}, Price: ${company.price}`);
+        });
+
+        res.render('displayResults', {results: results});
+    } catch (error) {
+        console.error("Error processing search:", error);
+    }
+})
